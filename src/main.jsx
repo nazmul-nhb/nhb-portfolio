16d6afcb05caed90131ccc8fe35f3a8ed812be06
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-05 00:28:54 +0600

    installed vercel speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -9,19 +9,21 @@ import './index.css';
 import ScrollButtons from './components/ScrollButtons/ScrollButtons';
 import AuthProvider from './providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-        <ScrollButtons />
-        <Toaster />
-      </HelmetProvider>
-    </AuthProvider>
+      <AuthProvider>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+          <ScrollButtons />
+          <SpeedInsights />
+          <Toaster />
+        </HelmetProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 )
