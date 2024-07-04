93a0f135897ba11306f206be01fc68f5e5c76222
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-07-05 02:50:24 +0600

    formatted root html file

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { router } from './routes/Routes';
-
-import './index.css';
 import ScrollButtons from './components/ScrollButtons/ScrollButtons';
 import AuthProvider from './providers/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SpeedInsights } from "@vercel/speed-insights/react";
+
+import './index.css';
 
 const queryClient = new QueryClient();
 
