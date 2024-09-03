ea962dcfff30e777f85a7d1505db6a84dc1a1332
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-03 19:50:34 +0600

    modularized icons data for skills and contacts

--- src/components/MovingContacts/MovingContacts.jsx
+++ src/components/MovingContacts/MovingContacts.jsx
@@ -1,53 +1,5 @@
 import { useState } from "react";
-import { FaDiscord, FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
-import { FaFacebookF, FaYahoo } from "react-icons/fa6";
-import { BiLogoGmail } from "react-icons/bi";
-import { SiLeetcode } from "react-icons/si";
-
-const icons = [
-	{
-		href: "https://t.me/nhb42",
-		component: FaTelegramPlane,
-		title: "Telegram",
-		color: "text-telegram",
-	},
-	{
-		href: "https://facebook.com/nazmul.batchu",
-		component: FaFacebookF,
-		title: "Facebook",
-		color: "text-facebook",
-	},
-	{
-		href: "mailto:nazmulnhb@gmail.com",
-		component: BiLogoGmail,
-		title: "Gmail",
-		color: "text-google",
-	},
-	{
-		href: "https://discord.com/users/831030314528538664",
-		component: FaDiscord,
-		title: "Discord",
-		color: "text-discord",
-	},
-	{
-		href: "https://leetcode.com/u/nazmul-nhb",
-		component: SiLeetcode,
-		title: "LeetCode",
-		color: "text-leetcode",
-	},
-	{
-		href: "https://wa.me/+8801623732187",
-		component: FaWhatsapp,
-		title: "WhatsApp",
-		color: "text-whatsapp",
-	},
-	{
-		href: "mailto:nazmulnhb007@yahoo.com",
-		component: FaYahoo,
-		title: "Yahoo! Mail",
-		color: "text-yahoo",
-	},
-];
+import { icons } from "../../data/data";
 
 const MovingContacts = () => {
 	const [isPaused, setIsPaused] = useState(false);
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,46 +1,5 @@
-import {
-	SiExpress,
-	SiMongodb,
-	SiFirebase,
-	SiTailwindcss,
-	SiReactquery,
-	SiRedux,
-	SiChakraui,
-	SiMui,
-	SiAxios,
-	SiPreact,
-	SiSass,
-	SiReacthookform,
-	SiJsonwebtokens,
-	SiAntdesign,
-	SiVercel,
-	SiNetlify,
-	SiNestjs,
-	SiMongoose,
-    SiJquery,
-    SiWebpack,
-} from "react-icons/si";
-import {
-	FaHtml5,
-	FaReact,
-	FaNodeJs,
-	FaFigma,
-	FaGitAlt,
-	FaSave,
-	FaAngular,
-	FaVuejs,
-	FaPython,
-	FaBootstrap,
-	FaDocker,
-	FaAws,
-} from "react-icons/fa";
-import { RiFlutterFill, RiJavascriptFill, RiNextjsLine } from "react-icons/ri";
+import { FaSave } from "react-icons/fa";
 import { MdDeleteForever, MdEditDocument } from "react-icons/md";
-import { TbBrandReactNative } from "react-icons/tb";
-import { BiLogoTypescript } from "react-icons/bi";
-import { IoLogoCss3 } from "react-icons/io5";
-import { BsStripe } from "react-icons/bs";
-import { GrMysql } from "react-icons/gr";
 import CountUp from "react-countup";
 import PropTypes from "prop-types";
 import { useInView } from "react-intersection-observer";
@@ -52,49 +11,7 @@ import Swal from "sweetalert2";
 import useGetSkills from "../../hooks/useGetSkills";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { Tooltip } from "react-tooltip";
-
-const skillIcons = {
-	HTML: <FaHtml5 />,
-	CSS: <IoLogoCss3 />,
-	SASS: <SiSass />,
-	JavaScript: <RiJavascriptFill />,
-	Python: <FaPython />,
-	React: <FaReact />,
-	Angular: <FaAngular />,
-	Flutter: <RiFlutterFill />,
-	"Vue.js": <FaVuejs />,
-	Preact: <SiPreact />,
-	TypeScript: <BiLogoTypescript />,
-	"Next.js": <RiNextjsLine />,
-	Redux: <SiRedux />,
-	"Redux Toolkit": <SiRedux />,
-	"React Native": <TbBrandReactNative />,
-	"Node.js": <FaNodeJs />,
-	NestJS: <SiNestjs />,
-	"Express.js": <SiExpress />,
-	MongoDB: <SiMongodb />,
-	Mongoose: <SiMongoose />,
-	TailwindCSS: <SiTailwindcss />,
-	Bootstrap: <FaBootstrap />,
-	Firebase: <SiFirebase />,
-	Git: <FaGitAlt />,
-	ChakraUI: <SiChakraui />,
-	MaterialUI: <SiMui />,
-	Axios: <SiAxios />,
-	Stripe: <BsStripe />,
-	Figma: <FaFigma />,
-	Docker: <FaDocker />,
-	Webpack: <SiWebpack />,
-	"React Hook Form": <SiReacthookform />,
-	JWT: <SiJsonwebtokens />,
-	jQuery: <SiJquery />,
-	"Ant Design": <SiAntdesign />,
-	MySQL: <GrMysql />,
-	"Vercel CLI": <SiVercel />,
-	Netlify: <SiNetlify />,
-	AWS: <FaAws />,
-	"TanStack Query": <SiReactquery />,
-};
+import { skillIcons } from "../../data/data";
 
 const Skills = ({ updateSkill }) => {
 	const [hoveredSkillId, setHoveredSkillId] = useState(null);
