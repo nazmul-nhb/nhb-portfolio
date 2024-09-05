278a7ce806c6d59d22170cf8222e642909869e58
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-05 19:44:30 +0600

    added animations in achievements page; reordered sections

--- src/pages/Achievements/Achievements.jsx
+++ src/pages/Achievements/Achievements.jsx
@@ -22,12 +22,64 @@ const Achievements = () => {
 			<Helmet>
 				<title>Achievements - Nazmul Hassan</title>
 			</Helmet>
+
+			{/* Certifications */}
+			<div id="education" className="scroll-margin-top mb-12">
+				<h2 className="pb-1 border-b my-6 font-bold text-xl sm:text-2xl md:text-3xl flex items-center gap-2">
+					<GrCertificate /> Certifications
+				</h2>
+				<div
+					data-aos="zoom-out-down"
+					data-aos-duration="1000"
+					className="space-y-4"
+				>
+					{certificates?.map((cert, idx) => (
+						<div
+							key={idx}
+							className="text-sm sm:text-base md:text-lg font-medium indent-2"
+						>
+							<PhotoProvider>
+								<div className="cursor-pointer hover:text-reddit transition-all duration-500">
+									<PhotoView src={cert.image}>
+										<h3 className="flex items-center text-base sm:text-lg md:text-xl font-semibold">
+											<LiaCertificateSolid className="text-lg sm:text-xl md:text-2xl animate-growShrink cursor-pointer" />
+											<span className="w-full overflow-x-auto whitespace-nowrap scrollbar-hide">
+												{cert.title}
+											</span>
+										</h3>
+									</PhotoView>
+								</div>
+							</PhotoProvider>
+
+							<h4 className="ml-[18px] sm:ml-5 md:ml-6">
+								<a
+									href={cert.link}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="hover:text-reddit transition-all duration-500"
+								>
+									{cert.organization}
+								</a>
+							</h4>
+							<h4 className="ml-[18px] sm:ml-5 md:ml-6">
+								{cert.duration}
+							</h4>
+						</div>
+					))}
+				</div>
+			</div>
+
 			{/* Published Packages */}
 			<div id="education" className="scroll-margin-top mb-12">
 				<h2 className="pb-1 border-b my-6 font-bold text-xl sm:text-2xl md:text-3xl flex items-center gap-2">
 					<TbBrandNpm /> Published Packages
 				</h2>
-				<div className="space-y-6">
+				<div
+					data-aos="zoom-in-up"
+					data-aos-duration="1000"
+					data-aos-delay="200"
+					className="space-y-6"
+				>
 					{packages?.map((pkg, idx) => (
 						<div
 							key={idx}
@@ -70,48 +122,6 @@ const Achievements = () => {
 								>
 									NPM Repository
 								</a>
-							</h4>
-						</div>
-					))}
-				</div>
-			</div>
-
-			{/* Certifications */}
-			<div id="education" className="scroll-margin-top mb-12">
-				<h2 className="pb-1 border-b my-6 font-bold text-xl sm:text-2xl md:text-3xl flex items-center gap-2">
-					<GrCertificate /> Certifications
-				</h2>
-				<div className="space-y-4">
-					{certificates?.map((cert, idx) => (
-						<div
-							key={idx}
-							className="text-sm sm:text-base md:text-lg font-medium indent-2"
-						>
-							<PhotoProvider>
-								<div className="cursor-pointer hover:text-reddit transition-all duration-500">
-									<PhotoView src={cert.image}>
-										<h3 className="flex items-center text-base sm:text-lg md:text-xl font-semibold">
-											<LiaCertificateSolid className="text-lg sm:text-xl md:text-2xl animate-growShrink cursor-pointer" />
-											<span className="w-full overflow-x-auto whitespace-nowrap overflow-ellipsis scrollbar-hide">
-												{cert.title}
-											</span>
-										</h3>
-									</PhotoView>
-								</div>
-							</PhotoProvider>
-
-							<h4 className="ml-[18px] sm:ml-5 md:ml-6">
-								<a
-									href={cert.link}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="hover:text-reddit transition-all duration-500"
-								>
-									{cert.organization}
-								</a>
-							</h4>
-							<h4 className="ml-[18px] sm:ml-5 md:ml-6">
-								{cert.duration}
 							</h4>
 						</div>
 					))}
